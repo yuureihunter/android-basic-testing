6f4b9b2ce39d0ea28979dfc1d56b7e3fa698da2a
somkiat <somkiat.p@gmail.com>
2017-11-09 23:36:52 +0700

    Start workshop

---
+++ app/src/main/java/cc/somkiat/basicunittesting/MainActivity.java
@@ -0,0 +1,22 @@
+package cc.somkiat.basicunittesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSaveClick(View view) {
+        //TODO
+    }
+
+    public void onRevertClick(View view) {
+        //TODO
+    }
+}
